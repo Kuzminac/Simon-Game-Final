d09784880a3f76571882ee215caad5bc6ae8436b
Nemanja <nemanjakuzminac21@gmail.com>
2022-06-24 11:40:53 +0200

    added start and play functions

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ let turn;
 let compTurn;
 let flash;
 
-const blueCard = document.querySelector(".blue")
-const redCard = document.querySelector(".red")
-const greenCard = document.querySelector(".green")
-const yellowCard = document.querySelector(".yellow")
-const onButton = document.querySelector(".on")
+const blueCard = document.querySelector(".blue");
+const redCard = document.querySelector(".red");
+const greenCard = document.querySelector(".green");
+const yellowCard = document.querySelector(".yellow");
+const onButton = document.querySelector(".on");
+const startButton = document.querySelector(".start");
+
+onButton.addEventListener('click', (event) => {
+    if (onButton.checked == true) {
+        on = true;
+    }
+    else {
+        on = false;
+    }
+});
+
+startButton.addEventListener('click', (event) => {
+    if (on) {
+        play()
+    }
+})
+
+function play() {
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    turn = 1;
+    correct = true;
+    for (let i = 0; i< 100; i++) {
+        compOrder.push(Math.floor(Math.random() * 4) + 1)
+    }
+    compTurn = true
+}
