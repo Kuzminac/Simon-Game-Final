0a447e8ce86a5f675bd992fa0c243ac5e274e11c
Nemanja <nemanjakuzminac21@gmail.com>
2022-06-24 11:34:00 +0200

    added variables

---
+++ script.js
@@ -0,0 +1,13 @@
+let on;
+let compOrder = [];
+let playerOrder = [];
+let correct;
+let turn;
+let compTurn;
+let flash;
+
+const blueCard = document.querySelector(".blue")
+const redCard = document.querySelector(".red")
+const greenCard = document.querySelector(".green")
+const yellowCard = document.querySelector(".yellow")
+const onButton = document.querySelector(".on")
